493b5dc73d257768f929279a97b93f81f950f2a1
SenseiTarzan <gabrielcaptari0@gmail.com>
2026-01-08 11:51:04 +0100

    fix: simplify index calculation in getCurrentWeather method

--- app/src/main/java/avaj/launcher/Class/Weather/WeatherProvider.java
+++ app/src/main/java/avaj/launcher/Class/Weather/WeatherProvider.java
@@ -15,10 +15,7 @@ public class WeatherProvider {
     public String getCurrentWeather(Coordinates p_coordinates) {
         int seed = p_coordinates.getLongitude() + p_coordinates.getLatitude() + p_coordinates.getHeight();
         int index = seed % 4;
-        if (index < 0) {
-            index += 4;
-        }
-        return weather[index];
+        return weather[Math.abs(index)];
     }
     
 }
